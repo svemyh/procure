8759b0a0d73f445e010e647192ca1675de2be45b
svemyh <s-myhre@outlook.com>
2024-10-10 19:04:25 -0700

    company.html, companies.html: should be working, but error 500

--- procureinsight/views.py
+++ procureinsight/views.py
@@ -5,8 +5,28 @@ from django.http import HttpResponse
 def index(request):
     return render(request, "index.html")
 
+
+def companies(request):
+    all_companies = [
+        {"company_name": "Google", "description": "Google is a search engine, created by Larry Page and Sergey Brin."},
+        {"company_name": "Meta", "description": "Social media platform, created by Mark Zuckerberg."},
+        {"company_name": "Palantir", "description": "Sell data analysis software to governments and corporations."},
+    ]
+    
+    return render(request, 'companies.html', {'all_companies': all_companies})
+
+
 def company(request):
-    return render(request, "company.html")
+
+    # Sample data to pass to the template
+    products_data = [
+        {"product": "Collapzable Card Example 1", "content": "This is the content for card 1."},
+        {"product": "Collapsable Card Example 2", "content": "This is the content for card 2."},
+        {"product": "Collapsable Card Example 3", "content": "This is the content for card 3."},
+        {"product": "Collapsable Card Example 4", "content": "This is the content for card 4."},
+    ]
+
+    return render(request, 'company.html', {'products_data': products_data})
 
 
 def login(request):
